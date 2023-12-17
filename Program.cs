9efb29e12fac3ca28bf412c6d7ac56cadf9fe75e
CaioRufino233 <130802702+CaioRufino233@users.noreply.github.com>
2023-12-17 14:49:12 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone  
+System.Console.WriteLine("Smartphone nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+nokia.ReceberLigacao();
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone iphone: ");
+Smartphone iphone = new Iphone (numero: "326536", modelo: "Modelo 2", imei: "3333333333", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+System.Console.WriteLine("\n");
+
+iphone.ReceberLigacao();
